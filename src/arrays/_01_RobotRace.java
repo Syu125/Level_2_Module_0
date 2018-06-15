382e2415f9a7f3dbe29b025b5777657709df5eed
Syu125 <league@WTS14.attlocal.net>
2018-06-14 18:26:09 -0700

    Trying to Figure out how to move the robots in a circle

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,35 +1,62 @@
 package arrays;
 
-
-import java.util.ArrayList;
+import java.util.Random;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
-	//1. make a main method
-		public static void main(String[] args) {
-			
-	//2. create an array of 5 robots.
-		Robot[]R = new Robot[5];
+	// 1. make a main method
+	public static void main(String[] args) {
+		Random r = new Random(50);
+		Boolean run = false;
+		int x = 50;
+		int centerX = 300;
+		int centerY = 300;
+		int moveX = 10;
+		// 2. create an array of 5 robots.
+		Robot[] R = new Robot[6];
 		Robot.setWindowSize(300, 300);
-	//3. use a for loop to initialize the robots.
-		for(int i = 0; i < R.length; i++) {
+		// 3. use a for loop to initialize the robots.
+		for (int i = 0; i < R.length; i++) {
 			R[i] = new Robot();
 		}
-		//4. make each robot start at the bottom of the screen, side by side, facing up
-		for(int j = 0;j<R.length; j++) {
-			R[j].mouseMove(20, 300);
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
+		for (int j = 0; j < R.length; j++) {
+			R[j].setX(x);
+			R[j].setY(550);
+			x += 100;
 		}
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
 
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+		/*
+		 * for (int k = 0; k < R.length; k++) { R[k].move(r.nextInt(100)); run = true; }
+		 */
 
-	//7. declare that robot the winner and throw it a party!
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
+		/*
+		 * while (true) { for (int k = 0; k < R.length; k++) {
+		 * R[k].move(r.nextInt(100)); run = true; // 7. declare that robot the winner
+		 * and throw it a party! if (R[k].getY() <= 50) { System.out.println("Robot " +
+		 * k + " wins!"); } } }
+		 */
+		while (true) {
+			for (int k = 0; k < R.length; k++) {
 
-	//8. try different races with different amounts of robots.
+				int moveY = (int) (Math.sqrt(100 - ((centerX - moveX) ^ 2)) - centerY);
+				run = true;
+				R[k].moveTo(moveX, moveY);
+				// 7. declare that robot the winner and throw it a party!
+				if (R[k].getY() <= 50) {
+					System.out.println("Robot " + k + " wins!");
+				}
+			}
+		}
 
-	//9. make the robots race around a circular track.
+		// 8. try different races with different amounts of robots.
+
+		// 9. make the robots race around a circular track.
+
+	}
 }
-}
-
